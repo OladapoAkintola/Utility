30c8589f82663ef7c5acc5d5847f0cc3be63a920
Oladapo <107548302+OladapoAkintola@users.noreply.github.com>
2025-09-23 13:59:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,45 +2,40 @@ import streamlit as st
 
 # *** PAGE SETUP ***
 vid_download_page = st.Page(
-    page = "views/video_downloader.py",
-    title = "Video Downloader",
-    icon = "⬇️",
-    default = True, 
+    page="views/video_downloader.py",
+    title="Video Downloader",
+    icon="⬇️",
+    default=True,
 )
 
 music_download_page = st.Page(
-    page = "views/music_downloader.py",
-    title = "Music Downloader",
-    icon = "🎵",
+    page="views/music_downloader.py",
+    title="Music Downloader",
+    icon="🎵",
 )
 
-
 scraper_page = st.Page(
-    page = "views/scraper.py",
-    title = "Email scraper",
-    icon = "🔍",
+    page="views/scraper.py",
+    title="Email scraper",
+    icon="🔍",
 )
 
 roaster_page = st.Page(
-    page = "views/roaster.py",
-    title = "Roaster Creator",
-    icon = "📃",
+    page="views/roaster.py",
+    title="Roaster Creator",
+    icon="📃",
 )
 
-#NAVIGATION WITH SECTIONS
+# NAVIGATION WITH SECTIONS
 pg = st.navigation({
-
     "MEDIA DOWNLOAD": [vid_download_page, music_download_page],
-    "SCRAPER": [scraper_page]
-    "Other Utilities": [roaster_page]
+    "SCRAPER": [scraper_page],
+    "OTHER UTILITIES": [roaster_page],
 })
 
-#SHARED ON ALL PAGES
+# SHARED ON ALL PAGES
 st.logo("static/2.jpg")
-st.sidebar.text("Made with ❤ by Dapo\nMorefeatures coming soon...")
+st.sidebar.text("Made with ❤ by Dapo\nMore features coming soon...")
 
-#RUN NAVIGATION
+# RUN NAVIGATION
 pg.run()
-
-
-
